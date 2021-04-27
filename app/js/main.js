f97b51e6fe1926ec73666375cbcbd1141a48ee4a
Rinkana1 <82525212+Rinkana1@users.noreply.github.com>
2021-04-27 06:34:26 -0400

    Update 4/27/21
    -More Style
    -Added Single Day calendar functionality
    -BUG: Cannot access single day calendar after changing the month and year, even if you change it back to the current month and year.

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,11 @@ $('#js-yearSelect').html(yearSelect);
 $('#js-confirmMonthYear').click(function () {
     $('#js-calendar').html(changeCalendar)
 });
+window.onscroll = function() {headerScroll()};
+$('.js-calendarDay').click(function() {
+    date = $(this).attr('data-id');
+    $('#js-calendar').html(dayCalendar(date))
+});
 
 
 function createCalendar() {
@@ -71,13 +76,15 @@ function createCalendar() {
             }
             if (week_day !== 7) {
                 // This Day
+                var data_day = (Calendar.getDate() < 10) ? "0" + Calendar.getDate():Calendar.getDate()
                 var day = Calendar.getDate();
-                var info = Calendar.getMonth() + 1 + '/' + day + '/' + Calendar.getFullYear();
+                var data_month = (Calendar.getMonth() < 10) ? "0" + (Calendar.getMonth() + 1):Calendar.getMonth()
+                var info = data_month + '/' + data_day + '/' + Calendar.getFullYear();
 
                 if (today === Calendar.getDate()) {
-                    html += '<td><a class="today_cal" href="#" data-id="' + info + '">' + day + '</a></td>';
+                    html += '<td><div class="today_cal js-calendarDay" data-id="' + info + '">' + day + '</div></td>';
                 } else {
-                    html += '<td><a href="#" data-id="' + info + '" >' + day + '</a></td>';
+                    html += '<td><div class="js-calendarDay" data-id="' + info + '" >' + day + '</div></td>';
                 }
             }
             if (week_day == 7) {
@@ -181,13 +188,15 @@ function changeCalendar() {
             }
             if (week_day !== 7) {
                 // This Day
+                var data_day = (Calendar.getDate() < 10) ? "0" + Calendar.getDate():Calendar.getDate()
                 var day = Calendar.getDate();
-                var info = Calendar.getMonth() + 1 + '/' + day + '/' + Calendar.getFullYear();
+                var data_month = (Calendar.getMonth() < 10) ? "0" + (Calendar.getMonth() + 1):Calendar.getMonth()
+                var info = data_month + '/' + data_day + '/' + Calendar.getFullYear();
 
                 if (today === Calendar.getDate()) {
-                    html += '<td><a class="today_cal" href="#" data-id="' + info + '">' + day + '</a></td>';
+                    html += '<td><div class="today_cal js-calendarDay" data-id="' + info + '">' + day + '</div></td>';
                 } else {
-                    html += '<td><a href="#" data-id="' + info + '" >' + day + '</a></td>';
+                    html += '<td><div class="js-calendarDay" data-id="' + info + '" >' + day + '</div></td>';
                 }
             }
             if (week_day == 7) {
@@ -198,4 +207,165 @@ function changeCalendar() {
         Calendar.setDate(Calendar.getDate() + 1);
     } //end for loop
     return html;
+}
+
+function dayCalendar(date) {
+    //Variables
+    var day_of_week = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+        month_of_year = new Array(
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ),
+        timeLibrary = new Array(
+            '12:00am',
+            '12:30am',
+            '1:00am',
+            '1:30am',
+            '2:00am',
+            '2:30am',
+            '3:00am',
+            '3:30am',
+            '4:00am',
+            '4:30am',
+            '5:00am',
+            '5:30am',
+            '6:00am',
+            '6:30am',
+            '7:00am',
+            '7:30am',
+            '8:00am',
+            '8:30am',
+            '9:00am',
+            '9:30am',
+            '10:00am',
+            '10:30am',
+            '11:00am',
+            '11:30am',
+            '12:00pm',
+            '12:30pm',
+            '1:00pm',
+            '1:30pm',
+            '2:00pm',
+            '2:30pm',
+            '3:00pm',
+            '3:30pm',
+            '4:00pm',
+            '4:30pm',
+            '5:00pm',
+            '5:30pm',
+            '6:00pm',
+            '6:30pm',
+            '7:00pm',
+            '7:30pm',
+            '8:00pm',
+            '8:30pm',
+            '9:00pm',
+            '9:30pm',
+            '10:00pm',
+            '10:30pm',
+            '11:00pm',
+            '11:30pm',
+        ),
+        Calendar = new Date(date),
+        year = Calendar.getYear(),
+        month = Calendar.getMonth(),
+        today = Calendar.getDate(),
+        weekday = day_of_week[Calendar.getDay()],
+        html = '';
+
+        //Template Calendar
+        html = '<table>';
+    //Head
+    html += '<thead class="time_cal">';
+    html += '<tr class="head_cal"><th colspan="3">' + weekday + ', ' + month_of_year[month] + ' ' + today + '</th></tr>';
+    html += '<tr class="subhead_cal"><th colspan="2">' + Calendar.getFullYear() + '</th><th class="subhead_cal" colspan="5"></th></tr>';
+    html += '</thead>'
+
+    //Body
+    html += '<tbody class="time_cal">'
+
+    for(var i = 0; i < 48; i++) {
+        html += `<tr id="time-${timeLibrary[i]}">`
+        html += `<td class="time"><div>${timeLibrary[i]}</div></td>`
+        html += '<td class="spacer"></td>'
+        html += '<td class="event"></td>'
+        html += '<td class="spacer"></td>'
+        html += '<td class="end"></td>'
+        html += '</tr>'
+    }
+    html += '</tbody>'
+    
+
+    return html;
+}
+
+function headerScroll() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $('header').css({
+            'height': '4.5vw',
+            'transition': '0.5s'
+        })
+        $('.title').css({
+            'font-size': '2vw',
+            'top': '1.25vw',
+            'line-height': '2vw',
+            'left': '1.5vw',
+            'transition': '0.5s'
+        })
+        $('.npt-dateSearch').css({
+            'top': '1.25vw',
+            'left': '16vw',
+            'height': '2vw',
+            'width': '20vw',
+            'font-size': '0.75vw',
+            'padding': '0 0.5vw 0 0.5vw',
+            'transition': '0.5s'
+        })
+        $('.btn-addEvent').css({
+            'top': '1.25vw',
+            'right': '1.5vw',
+            'height': '2vw',
+            'width': '5vw',
+            'font-size': '0.75vw',
+            'transition': '0.5s'
+        })
+    } else {
+        $('header').css({
+            'height': '12vw',
+            'transition': '0.5s'
+        });
+        $('.title').css({
+            'font-size': '3.5vw',
+            'line-height': '3.5vw',
+            'top': '4.25vw',
+            'left': '3vw'
+        });
+        $('.npt-dateSearch').css({
+            'top': '4.25vw',
+            'left': '28vw',
+            'height': '3.5vw',
+            'width': '30vw',
+            'font-size': '1.25vw',
+            'padding': '0 0.75vw 0 0.75vw',
+            'transition': '0.5s'
+        })
+        $('.btn-addEvent').css({
+            'top': '4.25vw',
+            'right': '3vw',
+            'height': '3.5vw',
+            'width': '8vw',
+            'font-size': '1.25vw',
+            'transition': '0.5s'
+        })
+    }
 }
